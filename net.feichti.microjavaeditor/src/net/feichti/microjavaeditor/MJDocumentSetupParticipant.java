5b64489343ab82c483518afe9eefa92392218ca9
Peter Feichtinger <shippo@gmx.at>
2014-10-14 10:35:19 +0000

    Change MJDocumentSetupParticipant to set the document partitioner for non-IDocumentExtension3 documents.

--- net.feichti.microjavaeditor/src/net/feichti/microjavaeditor/MJDocumentSetupParticipant.java
+++ net.feichti.microjavaeditor/src/net/feichti/microjavaeditor/MJDocumentSetupParticipant.java
@@ -10,13 +10,16 @@ public class MJDocumentSetupParticipant implements IDocumentSetupParticipant
 {
 	@Override
 	public void setup(IDocument document) {
+		MJPartitionScanner partitionScanner = MicroJavaEditorPlugin.getDefault().getPartitionScanner();
+		IDocumentPartitioner partitioner =
+				new FastPartitioner(partitionScanner, MJPartitionScanner.MICROJAVA_PARTITION_TYPES);
+		
 		if(document instanceof IDocumentExtension3) {
 			IDocumentExtension3 e3 = (IDocumentExtension3)document;
-			IDocumentPartitioner partitioner =
-					new FastPartitioner(MicroJavaEditorPlugin.getDefault().getPartitionScanner(),
-							MJPartitionScanner.MICROJAVA_PARTITION_TYPES);
 			e3.setDocumentPartitioner(MicroJavaEditorPlugin.MICROJAVA_PARTITIONING, partitioner);
-			partitioner.connect(document);
+		} else {
+			document.setDocumentPartitioner(partitioner);
 		}
+		partitioner.connect(document);
 	}
 }
