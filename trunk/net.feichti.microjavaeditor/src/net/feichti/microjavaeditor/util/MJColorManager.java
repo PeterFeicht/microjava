24d8dc8096af738434d2484045ade15765ad2901
pezo <pezo@47e3d007-e3f1-46e9-8a0e-73531e94c6b7>
2014-10-16 19:01:48 +0000

    Add a class for keyword style (color, bold, italic) to MJColorManager and use it in MJCodeScanner.

--- trunk/net.feichti.microjavaeditor/src/net/feichti/microjavaeditor/util/MJColorManager.java
+++ trunk/net.feichti.microjavaeditor/src/net/feichti/microjavaeditor/util/MJColorManager.java
@@ -4,12 +4,38 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.Token;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 
 public class MJColorManager
 {
+	public static class KeywordStyle
+	{
+		public final RGB color;
+		public final boolean bold;
+		public final boolean italic;
+		
+		public KeywordStyle(RGB color, boolean bold, boolean italic) {
+			this.color = color;
+			this.bold = bold;
+			this.italic = italic;
+		}
+		
+		public IToken getStyleToken(MJColorManager colorManager) {
+			int style = SWT.NORMAL;
+			style |= (bold ? SWT.BOLD : 0);
+			style |= (italic ? SWT.ITALIC : 0);
+			
+			TextAttribute attr = new TextAttribute(colorManager.getColor(color), null, style);
+			return new Token(attr);
+		}
+	}
+	
 	public static final RGB COMMENT = new RGB(128, 128, 0);
 	public static final RGB KEYWORD = new RGB(127, 0, 85);
 	public static final RGB IDENTIFIER = new RGB(0, 128, 255);
@@ -21,6 +47,18 @@ public class MJColorManager
 	public static final RGB OPERATOR = new RGB(128, 0, 255);
 	public static final RGB BRACKET = new RGB(0, 0, 0);
 	public static final RGB MAIN_METHOD = new RGB(0, 0, 0);
+	
+	public static final KeywordStyle COMMENT_STYLE = new KeywordStyle(COMMENT, false, false);
+	public static final KeywordStyle KEYWORD_STYLE = new KeywordStyle(KEYWORD, true, false);
+	public static final KeywordStyle IDENTIFIER_STYLE = new KeywordStyle(IDENTIFIER, false, false);
+	public static final KeywordStyle CONSTANT_STYLE = new KeywordStyle(CONSTANT, false, true);
+	public static final KeywordStyle TYPE_STYLE = new KeywordStyle(TYPE, false, true);
+	public static final KeywordStyle CHAR_CONST_STYLE = new KeywordStyle(CHAR_CONST, false, false);
+	public static final KeywordStyle NUMBER_STYLE = new KeywordStyle(NUMBER, false, false);
+	public static final KeywordStyle DEFAULT_STYLE = new KeywordStyle(DEFAULT, false, false);
+	public static final KeywordStyle OPERATOR_STYLE = new KeywordStyle(OPERATOR, false, false);
+	public static final KeywordStyle BRACKET_STYLE = new KeywordStyle(BRACKET, false, false);
+	public static final KeywordStyle MAIN_METHOD_STYLE = new KeywordStyle(MAIN_METHOD, true, false);
 	
 	protected Map<RGB, Color> mColorTable = new HashMap<>(10);
 	
